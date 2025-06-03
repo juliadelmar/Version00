28b5d46407a9ab34520779b19703f2e426b8cf1d
juliadelmar <149151461+juliadelmar@users.noreply.github.com>
2025-06-03 09:45:42 +0200

    Update AuthViewModel.kt

--- app/src/main/java/com/example/version00/ui/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/version00/ui/viewmodel/AuthViewModel.kt
@@ -1,3 +1,13 @@
+package com.example.version00.ui.viewmodel
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.messaging.FirebaseMessaging
+
 // Clase AuthViewModel que extiende de ViewModel
 class AuthViewModel : ViewModel() {
     // Instancia de FirebaseAuth para autenticar usuarios
